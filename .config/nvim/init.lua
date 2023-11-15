0d5c27aaeedcc9fd8f22687784de9355036410f3
EvgeniGenchev <genajok@gmail.com>
2023-11-15 07:39:52 +0100

    new nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,7 @@ o.dir = '.,,**'
 o.clipboard = "unnamedplus"
 set.termguicolors = true
 set.scrolloff = 5
+vim.cmd('set colorcolumn=100')
 
 --------------lightline----------------------------
 
@@ -40,9 +41,6 @@ Plug 'EvgeniGenchev/ducky-vim'
 Plug 'itchyny/lightline.vim'
 Plug 'cespare/vim-toml'
 Plug 'preservim/nerdtree'
-Plug 'neovim/nvim-lspconfig'
-Plug 'tami5/lspsaga.nvim'
-Plug 'preservim/nerdtree'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'wilriker/gcode.vim'
@@ -55,28 +53,36 @@ Plug 'EvgeniGenchev/comment-nvim'
 Plug 'norcalli/nvim-colorizer.lua'
 Plug 'nvim-tree/nvim-web-devicons'
 Plug 'goolord/alpha-nvim'
+Plug 'zah/nim.vim'
+Plug 'iamcco/markdown-preview.nvim'
+Plug 'neoclide/coc.nvim'
 
 vim.call('plug#end')
 -----------end-vim-plug-----------------------------
 
+-- Plug 'neovim/nvim-lspconfig'
+-- Plug 'tami5/lspsaga.nvim'
+
+require"ibl".setup()
 
 -----------lspconfig--------------------------------
 
-require'lspconfig'.pyright.setup{ignore={"**/.config/qutebrowser/config.py"},}
-require'lspconfig'.nimls.setup{}
-require'lspconfig'.lua_ls.setup{
-	settings = {
-		Lua = {
-			diagnostics = {globals = {'vim'},},
-			telemetry = {enable = false,},
-		},
-	},
-}
-require'lspconfig'.ccls.setup{}
-require'lspconfig'.bashls.setup{}
+-- require'lspconfig'.pyright.setup{ignore={"**/.config/qutebrowser/config.py"},}
+-- require'lspconfig'.nimls.setup{}
+-- require'lspconfig'.lua_ls.setup{
+	-- settings = {
+		-- Lua = {
+			-- diagnostics = {globals = {'vim'},},
+			-- telemetry = {enable = false,},
+		-- },
+	-- },
+-- }
+-- require'lspconfig'.ccls.setup{}
+-- require'lspconfig'.bashls.setup{}
+-- require'lspconfig'.zls.setup{}
+
 
 -------end-lspconfig--------------------------------
-
 
 ---------treesitter--------------------------------
 require'nvim-treesitter.configs'.setup {
@@ -92,23 +98,66 @@ require'nvim-treesitter.configs'.setup {
 -------end-treesitter------------------------------
 
 -------lspsaga--------------------------------------
-
-local saga = require'lspsaga'
-
-saga.init_lsp_saga {
-	border_style = "round",
-}
--- sets the background of the diagnostic windows 
-run("hi LspFloatWinNormal guibg=#272727")
-
-local smap = vim.api.nvim_buf_set_keymap
-smap(0, "n", "K",  ":Lspsaga hover_doc<cr>", {silent = true, noremap = true})
-smap(0, "n", "go", "<cmd>Lspsaga show_line_diagnostics<cr>", {silent = true, noremap = true})
-smap(0, "n", "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", {silent = true, noremap = true})
-smap(0, "n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", {silent = true, noremap = true})
-smap(0, "n", "gx", "<cmd>Lspsaga code_action<cr>", {silent = true, noremap = true})
---smap(0, "n", "gs", "<cmd>Lspsaga signiture_help<cr>", {silent = true, noremap = true})
+-- 
+-- local saga = require'lspsaga'
+-- 
+-- saga.init_lsp_saga {
+	-- border_style = "round",
+-- }
+-- sets the background of the diagnostic windows
+-- run("hi LspFloatWinNormal guibg=#272727")
+-- 
+-- local smap = vim.api.nvim_buf_set_keymap
+-- smap(0, "n", "K",  ":Lspsaga hover_doc<cr>", {silent = true, noremap = true})
+-- smap(0, "n", "go", "<cmd>Lspsaga show_line_diagnostics<cr>", {silent = true, noremap = true})
+-- smap(0, "n", "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", {silent = true, noremap = true})
+-- smap(0, "n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", {silent = true, noremap = true})
+-- smap(0, "n", "gx", "<cmd>Lspsaga code_action<cr>", {silent = true, noremap = true})
+-- map(0, "n", "gs", "<cmd>Lspsaga signiture_help<cr>", {silent = true, noremap = true})
 ---end-lspsaga--------------------------------------
+
+-------coc--------------------------------------
+set.backup = false
+set.writebackup = false
+set.updatetime = 300
+
+local keyset = vim.keymap.set
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
+-- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list
+keyset("n", "gk", "<Plug>(coc-diagnostic-prev)", {silent = true})
+keyset("n", "gj", "<Plug>(coc-diagnostic-next)", {silent = true})
+
+-- GoTo code navigation
+keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
+keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
+
+keyset("n", "<M>rn", "<Plug>(coc-rename)", {silent = true})
+---end-coc--------------------------------------
 
 
 ------------key-mapping-----------------------------
@@ -135,6 +184,11 @@ map('mn', ':NERDTree<CR>')
 
 --------end-key-mapping-----------------------------
 require('smokinq')
-require('comment').setup()
+require('comment').setup({
+	languages = {
+		sh = "#",
+		nim = "#",
+	},
+})
 require('colorizer').setup()
 require'alpha'.setup(require'alpha.themes.startify'.config)
