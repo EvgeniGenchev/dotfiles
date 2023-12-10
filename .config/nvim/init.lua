a4eea1c78e730caea20b9835d417067af76aa72f
EvgeniGenchev <genajok@gmail.com>
2023-12-10 23:36:22 +0100

    new nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,12 @@
+local vim = vim -- luacheck: ignore
 local w = vim.wo
 local o = vim.o
 -- local b = vim.bo
 local set = vim.opt
 local run = vim.cmd
 
+set.foldmethod = "expr"
+set.foldexpr = "nvim_treesitter#foldexpr()"
 set.tabstop = 4
 set.softtabstop = 4
 set.shiftwidth = 4
@@ -95,6 +98,37 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
+
+vim.api.nvim_create_autocmd({"BufRead", "BufWinEnter"}, {
+    pattern = "*", -- This pattern applies to all files
+    command = "normal! zR"
+})
+
+-- vocal api = vim.api
+-- local M = {}
+-- function to create a list of commands and convert them to autocommands
+----- This function is taken from https://github.com/norcalli/nvim_utils
+-- function M.nvim_create_augroups(definitions)
+    -- for group_name, definition in pairs(definitions) do
+        -- api.nvim_command('augroup '..group_name)
+        -- api.nvim_command('autocmd!')
+        -- for _, def in ipairs(definition) do
+            -- local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+            -- api.nvim_command(command)
+        -- end
+        -- api.nvim_command('augroup END')
+    -- end
+-- end
+-- 
+-- 
+-- local autoCommands = {
+    -- other autocommands
+    -- open_folds = {
+        -- {"BufReadPost,FileReadPost", "*", "normal zR"}
+    -- }
+-- }
+-- 
+-- M.nvim_create_augroups(autoCommands)
 -------end-treesitter------------------------------
 
 -------lspsaga--------------------------------------
@@ -188,6 +222,7 @@ require('comment').setup({
 	languages = {
 		sh = "#",
 		nim = "#",
+		zig = "//",
 	},
 })
 require('colorizer').setup()
